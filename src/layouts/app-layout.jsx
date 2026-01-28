25568516a81e62dee624b984a7354d0c2dde5750
goutamiydamodar7-lab <goutamiydamodar7@gmail.com>
2026-01-28 14:25:02 +0530

    create

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -4,17 +4,37 @@ import SidebarNav from "@/components/sidebar-nav";
 import { PlusIcon, Sidebar } from "lucide-react";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import useCreateThread from "@/Hooks/use-create-thread";
 
 const AppLayout = () => {
+  const thread = useCreateThread();
   return (
     <div className="app">
       <main>
-        <SidebarNav />
+        <SidebarNav onCreateClick={thread.openDialog}/>
         <Header />
         <div className="w-full md:max-w-4xl mx-auto bg-red-400">
           <Outlet />
         </div>
         <BottomNav />
+        <Dialog open={thread.open} onOpenChange={thread.setOpen}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </main>
       <div className="hidden md:flex fixed bottom-8 right-8">
         <div className="border border-gray-200 px-6 py-4 rounded-lg hover:shadow-sm cursor-pointer group bg-white">
