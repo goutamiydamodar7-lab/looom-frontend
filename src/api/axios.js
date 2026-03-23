81187173843536a678c6fd5b336ef886204101b4
goutamiydamodar7-lab <goutamiydamodar7@gmail.com>
2026-03-23 14:56:25 +0530

    ok

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
   timeout: 10000, // prevent hanging requests
 });
 
-// Attach token
+// Attach tokensss
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
