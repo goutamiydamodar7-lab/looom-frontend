50937bef17859cf3c4873826416fee1f5db77e0a
goutamiydamodar7-lab <goutamiydamodar7@gmail.com>
2026-03-25 13:18:28 +0530

    hanelelogout

--- src/components/user-menu.jsx
+++ src/components/user-menu.jsx
@@ -11,7 +11,9 @@ import { Link } from "react-router-dom";
 const UserMenu = ({ size = 24, className = "", align = "start" }) => {
   const handleLogout = () => {
     logoutUser();
+    window.location.href = "/";
   };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
